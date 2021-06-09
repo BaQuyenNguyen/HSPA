5033421520f5ea3f7d551f8b562a4e442444eb76
BaQuyenNguyen <baquyennguyen94@gmail.com>
2021-06-09 18:44:01 +0800

    update user register UI, add notification dialog

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AddPropertyComponent } from './property/add-property/add-property.compo
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserService } from './services/user.service';
+import { AlertifyService } from './services/alertify.service';
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent },
@@ -44,7 +46,11 @@ const appRoutes: Routes = [
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
   ],
-  providers: [HousingService],
+  providers: [
+    HousingService,
+    UserService,
+    AlertifyService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,53 +1,93 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-register.component.scss']
+  styleUrls: ['./user-register.component.scss'],
 })
 export class UserRegisterComponent implements OnInit {
-
   registerationForm: FormGroup;
-  constructor() { }
+  user: User;
+  userSubmitted: boolean;
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private alertify: AlertifyService
+    ) {}
 
   ngOnInit() {
-    this.registerationForm = new FormGroup({
-      userName: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      confirmPassword: new FormControl(null, Validators.required),
-      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
-    }, this.passwordMatchingValidator);
+    this.buildRegisterationForm();
   }
 
-  passwordMatchingValidator(fg: FormGroup): Validators{
-    return fg.get('password').value === fg.get('confirmPassword').value ? null :
-    {notmatched: true}
+  buildRegisterationForm() {
+    this.registerationForm = this.fb.group(
+      {
+        userName: [null, Validators.required],
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required, Validators.minLength(8)]],
+        confirmPassword: [null, Validators.required],
+        mobile: [null, [Validators.required, Validators.maxLength(10)]],
+      },
+      { validators: this.passwordMatchingValidator }
+    );
   }
 
-  get userName(){
+  passwordMatchingValidator(fg: FormGroup): Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value
+      ? null
+      : { notmatched: true };
+  }
+
+  onSubmit() {
+    console.log(this.registerationForm);
+    this.userSubmitted = true;
+    if (this.registerationForm.valid) {
+      // this.user = Object.assign(this.user, this.registerationForm.value);
+      this.userService.addUser(this.userData());
+      this.registerationForm.reset();
+      this.userSubmitted = false;
+      this.alertify.success('Congrats, you are successfully registered');
+    } else {
+      this.alertify.error('Kindly provide the required fields');
+    }
+  }
+
+  userData() {
+    return (this.user = {
+      userName: this.userName.value,
+      email: this.email.value,
+      password: this.password.value,
+      mobile: this.mobile.value,
+    });
+  }
+
+  get userName() {
     return this.registerationForm.get('userName') as FormControl;
   }
 
-  get email(){
+  get email() {
     return this.registerationForm.get('email') as FormControl;
   }
 
-  get password(){
+  get password() {
     return this.registerationForm.get('password') as FormControl;
   }
 
-  get confirmPassword(){
+  get confirmPassword() {
     return this.registerationForm.get('confirmPassword') as FormControl;
   }
 
-  get mobile(){
+  get mobile() {
     return this.registerationForm.get('mobile') as FormControl;
   }
-
-  onSubmit(){
-    console.log(this.registerationForm);
-  }
-
 }
